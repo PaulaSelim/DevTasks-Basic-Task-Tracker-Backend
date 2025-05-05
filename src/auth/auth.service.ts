4a08177e7129d24fddfcf1b07bb3f2b344be6e17
MirnaMilad <mirnamilad0101@gmail.com>
2025-05-05 15:54:55 +0300

    refactor: remove AppController and AppService; add Auth, User, and Task modules

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class AuthService {
+  signUp() {
+    return { msg: "I am sign up" };
+  }
+  signIn() {
+    return { msg: "I am sign in" };
+  }
+}
